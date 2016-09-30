5791ab30fcb45e096c72c77098d31c563507944a
Josh Malmquist <joshmalmquist96@gmail.com>
2016-09-30 15:03:49 -0400

    Setup Basic Framework for Menus
    I create the main menu and option menu scenes and created the UI for the
    menus. I made a function to load unity scenes. Currently the start game,
    options, main menu btns all function and go to there respective scenes.
    I also fixed the camera on the test level scene to use the smoothfollow
    script, and I added a player script for noah to reference

---
+++ Assets/Scripts/MenuScript.cs
@@ -0,0 +1,25 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class MenuScript : MonoBehaviour {
+	
+    // Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+
+    //loads a unity scene 
+    public void LoadScene(string sceneName){ //NOTE: scene must be in the build to be able to go to that scene
+        Application.LoadLevel(sceneName);
+    }
+
+    public void StartGame()
+    {
+        LoadScene("Test Level");
+    }
+}
