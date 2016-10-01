15fa781a91641f46680fcf26c9b85cb3e945c604
Jason Noone <jdn1885@ad.rit.edu>
2016-10-01 15:48:19 -0400

    Jason's changes
    Added Evnironment assets and an object script with stuff in it, but if
    more is required let me know.

---
+++ Assets/Scripts/Object.cs
@@ -0,0 +1,38 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Object : MonoBehaviour {
+
+    private bool hazardous = false; //is the object harmful to the player
+    private int damage = 0; //if it is hazardous how much damage will it do the player
+    private bool moveable = false; //can the player move the object
+    private GameObject obj;
+
+	// Use this for initialization
+	void Start () {
+	    
+	}
+	
+	// Update is called once per frame
+	void Update () {
+
+	}
+
+    //destroys the fobject
+    private void DestroyObject()
+    {
+        Destroy(obj);
+    }
+
+    //get damage
+    public int Damage()
+    {
+        return damage;
+    }
+
+    //get hazardous
+    public bool Hazardous()
+    {
+        return hazardous;
+    }
+}
