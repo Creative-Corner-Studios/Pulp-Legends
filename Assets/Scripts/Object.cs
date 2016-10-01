8fe031d634d0e93f3905fd51f9e2f2541c71a285
Jason Noone <jdn1885@ad.rit.edu>
2016-10-01 18:53:54 -0400

    Added some more Enviroment Assets and Updated Object script

--- Assets/Scripts/Object.cs
+++ Assets/Scripts/Object.cs
@@ -1,16 +1,25 @@
 ﻿using UnityEngine;
 using System.Collections;
 
+//requires the object to have a collider
+[RequireComponent(typeof(Collider2D))]
 public class Object : MonoBehaviour {
 
     private bool hazardous = false; //is the object harmful to the player
     private int damage = 0; //if it is hazardous how much damage will it do the player
-    private bool moveable = false; //can the player move the object
+    private bool moveable = true; //can the player move the object
     private GameObject obj;
+    private Rigidbody2D objRigid;
 
 	// Use this for initialization
 	void Start () {
-	    
+        obj = gameObject;
+	    if(moveable == true)
+        {
+            objRigid = obj.AddComponent<Rigidbody2D>();
+            objRigid.mass = 5;
+            
+        }
 	}
 	
 	// Update is called once per frame
