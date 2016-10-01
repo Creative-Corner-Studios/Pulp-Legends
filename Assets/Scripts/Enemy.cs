6935519fb48e2793f2b98529338da6e17594f8a8
ndp4918 <ndp4918@rit.edu>
2016-10-01 16:07:46 -0400

    Noah's work for 10/3
    Added world object and also started on enemy script. Figured out how to
    keep an object between scenes but not tested yet.

---
+++ Assets/Scripts/Enemy.cs
@@ -0,0 +1,64 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Enemy : MonoBehaviour {
+
+    private int health;
+    private enum enemyType; //we should create a state machine so we can distinguish enemy types
+    private float attackPower;
+    private float speed;
+    private GameObject enemy;
+    private GameObject Bullet;
+    private int detectRange;
+
+
+    // Use this for initialization
+    void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+
+    //prooperites
+    public int Health
+    {
+        get
+        {
+            return health;
+        }
+        set
+        {
+            health = value;
+        }
+    }
+
+    public void modHealth(int mod)  // add/subtract from health
+    {
+        health += mod;
+    }
+
+    private void Sentry() // this method will have the enemy patrol for player
+    {
+
+    }
+
+    private bool DetectPlayer(Player p)  // this method checks if player is within enemy’s detection range.
+    {
+        float xDis = Mathf.Abs(p.gameObject.transform.position.x - gameObject.transform.position.x);
+        float yDis = Mathf.Abs(p.gameObject.transform.position.y - gameObject.transform.position.y);
+        float distance = Mathf.Sqrt((xDis * xDis) + (yDis * yDis));
+        if(distance <= detectRange)//close enough to be detected
+        {
+            return true;
+        }
+        return false;
+    }
+
+    private void DestroyEnemy() // destroys the gameobject
+    {
+        Destroy(gameObject);
+    }
+}
---
+++ Assets/Scripts/WorldController.cs
@@ -0,0 +1,56 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class WorldController : MonoBehaviour {
+
+    public GameObject P1;
+    public GameObject P2;
+    Camera GameCamera;
+    MenuScript Menues;
+
+    void Awake()//happens once, even if the script is not active
+    {
+        DontDestroyOnLoad(GameObject.Find("WorldController"));
+    }
+
+	// Use this for initialization
+	void Start () {
+        P1 = null;
+        P2 = null;
+        GameCamera = Camera.current;
+        Menues = new MenuScript();
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        switch (Application.loadedLevelName)
+        {
+            case "Main Menu":
+                break;
+            case "Option Menu":
+                break;
+            case "Test Level":
+                break;
+            case "Level 1":
+                break;
+            default:
+                break;
+        }
+    }
+
+    public void GameOver() // this will end the game
+    {
+        
+    }
+
+    public void PauseGame() // pause game and opens pause menu
+    {
+
+    }
+
+    public void WinGame() // done when you win the game
+    {
+        
+    }
+
+}
