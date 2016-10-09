d074b3d433cdd2b516403719b72b805a954c6556
Josh Malmquist <joshmalmquist96@gmail.com>
2016-10-09 16:47:35 -0400

    Added Goal Prefab
    I made a goal prefab to move the players to next level when it is
    touched. Fun fact the goal is the Tardis( iwill replace it with a better
    quality image later). I changed the level 1 scene name from "jason's
    test scene" to "level 1". I also added level 1 to build. I still need to
    implement the code for setting up characters in level 1

---
+++ Assets/Scripts/Goal.cs
@@ -0,0 +1,55 @@
+﻿using UnityEngine;
+using System.Collections;
+
+[RequireComponent(typeof(Collider2D))]
+public class Goal : MonoBehaviour {
+    //attributes
+    WorldController worldControl;
+    [SerializeField] private WorldController.Screen nextLevel;
+    private string level;
+
+	// Use this for initialization
+	void Start () {
+        worldControl = GameObject.Find("WorldController").GetComponent<WorldController>();
+
+        switch (nextLevel)
+        {
+            case WorldController.Screen.MAINMENU:
+                level = "Main Menu";
+                break;
+
+            case WorldController.Screen.TESTLEVEL:
+                level = "Test Level";
+                break;
+
+            case WorldController.Screen.LEVEL1:
+                level = "Level 1";
+                break;
+
+            case WorldController.Screen.OPTIONMENU:
+                level = "Option Menu";
+                break;
+        }
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+
+    void OnTriggerEnter2D(Collider2D thing)
+    {
+        Debug.Log("Goal triggered");
+        if (thing.tag == "Player")
+        {
+            NextLevel();
+        }
+    }
+
+    void NextLevel()
+    {
+        worldControl.runSetup = true;
+        worldControl.currentScreen = nextLevel;
+        Application.LoadLevel(level);
+    }
+}
